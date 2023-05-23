e4980366f0cdcb3e122efceaccfe4204d9cec06b
Rusu Mihail <93447754+WorkHard7@users.noreply.github.com>
2023-05-23 12:57:07 +0300

    - Adds logic for watchtower component (#5)
    Adds logic for watchtower component
    Implements a css custom spinner when data is loading
    
    Co-authored-by: PullRequestOpened

--- src/app/components/Watchtower/watchtower-paragraph/watchtower-paragraph.component.ts
+++ src/app/components/Watchtower/watchtower-paragraph/watchtower-paragraph.component.ts
@@ -1,9 +1,53 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CountdownService} from "../../../services/countdown.service";
 
 @Component({
   selector: 'app-watchtower-paragraph',
   templateUrl: './watchtower-paragraph.component.html',
   styleUrls: ['./watchtower-paragraph.component.scss']
 })
-export class WatchtowerParagraphComponent {
+export class WatchtowerParagraphComponent implements OnInit, AfterViewInit {
+  startTime: Date = new Date();
+  endTime: Date = new Date();
+
+  paragraph!: number;
+  currentParagraph: number = 0;
+  isLoading: boolean = true;
+
+  constructor(
+    private route: ActivatedRoute,
+    public countdownService: CountdownService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(param => {
+      const paragraph = param.get('paragraph');
+      if (paragraph) {
+        this.paragraph = +paragraph;
+      }
+    })
+  }
+
+  ngAfterViewInit(): void {
+    this.startTime.setHours(9,58,0);
+    this.endTime.setHours(10, 58, 0);
+    setTimeout(() => {
+      this.countdownService.startCountdown(this.endTime);
+    });
+
+    setInterval(() => {
+      const currentTime = new Date();
+
+      // remained time for orator when beginning the speech
+      const remainingTime = (this.endTime.getTime() - currentTime.getTime()) / 60000;
+
+      // what should be paragraph duration based on number of paragraphs selected
+      const paragraphDuration = 60 / this.paragraph;
+
+      this.currentParagraph = Math.floor((60 - remainingTime) / paragraphDuration) + 1;
+      this.isLoading = false;
+    },1000)
+  }
 }
