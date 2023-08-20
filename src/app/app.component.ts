ffe0cdd95f2c2a9e44dd968e3e8dc8ea96eda647
WorkHard7 <solinuxnet@gmaill.com>
2023-08-20 15:16:33 +0300

    - adding a smooth opacity transition effect  on main screen - updates docs folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import {Component} from '@angular/core';
-import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  constructor(public router: Router) {
-  }
 }
