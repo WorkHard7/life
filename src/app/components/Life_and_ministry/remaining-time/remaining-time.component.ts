8fd8de8f4f5f92eecf54ed25f505eb2fe208de9c
WorkHard7 <solinuxnet@gmaill.com>
2024-02-29 15:41:03 +0200

    - finish part will show just remaining time clock..but will keep the same position

--- src/app/components/Life_and_ministry/remaining-time/remaining-time.component.ts
+++ src/app/components/Life_and_ministry/remaining-time/remaining-time.component.ts
@@ -1,21 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {CountdownService} from "../../../services/countdown.service";
 import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 import {Router} from "@angular/router";
 import {CountdownAllocatedTimeService} from "../../../services/countdown-allocated-time.service";
 import {SelectedSpeechService} from "../../../services/selected-speech.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-remaining-time',
   templateUrl: './remaining-time.component.html',
   styleUrls: ['./remaining-time.component.scss']
 })
-export class RemainingTimeComponent implements OnInit {
+export class RemainingTimeComponent implements OnInit, OnDestroy {
   @Input() introduction: boolean = false;
   @Input() finish: boolean = false;
 
   protected readonly faArrowLeft = faArrowLeft;
   redColorText: boolean = false;
+
+  private selectedSpeechSubscription!: Subscription;
 
   constructor(
     public countdownService: CountdownService,
@@ -43,11 +46,23 @@ export class RemainingTimeComponent implements OnInit {
   }
 
   updateRemainingTimeIfNecessary(): boolean {
-    if (this.countdownService.remainingTime > this.countdownAllocatedTimeService.remainingAllocatedTime) {
-      this.countdownService.remainingTime = this.countdownAllocatedTimeService.remainingAllocatedTime + 1;
-      console.log('countdownService remainingTime is bigger than countdownAllocatedTimeService');
-    }
+    this.selectedSpeechSubscription = this.selectedSpeechService.selectedSpeech$.subscribe(selectedSpeech => {
+      if (selectedSpeech.title === 'Cuvinte de încheiere, anunțuri') {
+        return;
+      }
+
+      if (this.countdownService.remainingTime > this.countdownAllocatedTimeService.remainingAllocatedTime) {
+        this.countdownService.remainingTime = this.countdownAllocatedTimeService.remainingAllocatedTime + 1;
+        console.log('countdownService remainingTime is bigger than countdownAllocatedTimeService');
+      }
+    })
 
     return true;
+  }
+
+  ngOnDestroy(): void {
+    if (this.selectedSpeechSubscription) {
+      this.selectedSpeechSubscription.unsubscribe();
+    }
   }
 }
