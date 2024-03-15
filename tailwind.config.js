9e0523fe0d3d7ab83a537b0ce90e44c2bc3c6441
WorkHard7 <solinuxnet@gmaill.com>
2024-03-15 16:19:12 +0200

    - fixes module file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/index.html",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
