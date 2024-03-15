2a476ca637cd1f7fe59f27c8392e8d02ac85d0c5
WorkHard7 <solinuxnet@gmaill.com>
2024-03-15 15:33:30 +0200

    - fixes module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,15 +88,7 @@ import {StoreDevtoolsModule} from "@ngrx/store-devtools";
     FontAwesomeModule,
     FormsModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-    }),
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
