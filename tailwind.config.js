180378994852c089ae0103d3ba30135d2e9e3a40
WorkHard7 <solinuxnet@gmaill.com>
2024-03-15 16:52:15 +0200

    - fixes package.json file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/index.html",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
